8ee5d536317ff7df37ff90bae69ad0930ee80742
Jamie <jamieh028@gmail.com>
2024-07-24 10:15:26 -0400

    add dataset summary

--- dashboard.py
+++ dashboard.py
@@ -21,7 +21,6 @@ selected_df = dataframes[selected_df_name]
 
 # filter the dataset
 st.header("Dataset Filters")
-selected_columns = st.multiselect('select columns:', selected_df.columns.tolist())
 
 country_codes = {
     'Republic of Burundi': 'BDI',	
@@ -46,12 +45,35 @@ country_codes = {
 selected_countries = st.multiselect('select countries:', list(country_codes.keys()))
 selected_country_codes = [country_codes[country] for country in selected_countries]
 
+selected_columns = st.multiselect('select columns:', selected_df.columns.tolist(), selected_df.columns.tolist())
+
+
+# summary section
+with st.expander("show a summary of the filtered dataset"):
+
+    summary = selected_df[selected_columns].describe()
+
+    columns = summary.columns
+    tabs = st.tabs(columns.to_list())
+
+    for i, column in enumerate(columns):
+        with tabs[i]:
+            st.subheader(f"summary of: {column}")
+            st.markdown(f"**Count**: {summary.loc['count', column]}")
+            st.markdown(f"**Mean**: {summary.loc['mean', column]:.2f}")
+            st.markdown(f"**Standard Deviation**: {summary.loc['std', column]:.2f}")
+            st.markdown(f"**Min**: {summary.loc['min', column]}")
+            st.markdown(f"**25th Percentile**: {summary.loc['25%', column]}")
+            st.markdown(f"**50th Percentile (Median)**: {summary.loc['50%', column]}")
+            st.markdown(f"**75th Percentile**: {summary.loc['75%', column]}")
+            st.markdown(f"**Max**: {summary.loc['max', column]}")
+
 
 # create the dataframe profile and display it
+st.header("Profile Report")
 if st.button('Generate Profile Report'):
     if selected_df[selected_columns].empty:
         st.write("no data available for the subsetted data.")
     else:
-        st.header("Profile Report")
-        profile = ProfileReport(selected_df[selected_columns], title=f"Profile Report for {selected_df_name}", explorative=False)
+        profile = ProfileReport(selected_df[selected_columns], title=f"Profile Report for {selected_df_name}", explorative=True)
         st_profile_report(profile)
--- preprocess.py
+++ preprocess.py
@@ -5,6 +5,12 @@ def preprocess(df_path):
     # create data fram
     df = pd.read_csv(df_path)
 
+    print(df.describe())
+
     # process data as needed here
 
     return df
+
+
+if __name__ == '__main__':
+    preprocess('data/education_test.csv')
