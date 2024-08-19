3641232de475806c7a12dd76e50bbbc1a2d91ba6
Jamie <jamieh028@gmail.com>
2024-08-19 11:09:51 -0400

    add azure llm

--- dashboard.py
+++ dashboard.py
@@ -145,8 +145,8 @@ if st.button('Get Response'):
                 azure = AzureOpenAI(
                     api_token=st.secrets["azure"],
                     azure_endpoint="https://openai-osaa-v2.openai.azure.com/",
-                    api_version="2023-05-15",
-                    deployment_name="my-deployment-name"
+                    api_version="2024-05-15",
+                    deployment_name="gpt40"
                 )
                 smart_df = SmartDataframe(df[selected_columns], config={"llm": azure})
                 response = smart_df.chat(query)
