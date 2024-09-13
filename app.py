0047c2dcb80beedeaa59d76b04c002c0d98052b8
Jamie <jamieh028@gmail.com>
2024-09-13 10:17:32 -0400

    add password protection

--- app.py
+++ app.py
@@ -1,5 +1,53 @@
 import streamlit as st
 from helper_functions import setup_db
+import hmac
+
+
+# check password
+def check_password():
+    """
+    Returns `True` if the user had the correct password.
+    """
+
+    def password_entered():
+        """
+        Checks whether a password entered by the user is correct.
+        """
+
+        if hmac.compare_digest(st.session_state["password"], st.secrets["password"]):
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]  # remove the password
+        else:
+            st.session_state["password_correct"] = False
+
+    # return True if the password is validated.
+    if st.session_state.get("password_correct", False):
+        return True
+
+    # show input for password.
+    col1, col2 = st.columns(2)
+    with col1:
+        st.image("content/OSAA-Data-logo.svg")
+
+
+    st.title("SMU's Data App")
+    st.markdown("Welcome to the Office of the Speical Advisor to Africa's Strategic Management Unit's Data App. Please enter the app password to access the data app.")
+
+    st.markdown("<hr>", unsafe_allow_html=True)
+    st.write("")
+
+    st.text_input(
+        "Password", type="password", on_change=password_entered, key="password"
+    )
+
+    if "password_correct" in st.session_state:
+        st.error("😕 Password incorrect")
+    return False
+
+
+if not check_password():
+    st.stop()
+
 
 # create test database
 db_path = setup_db()
