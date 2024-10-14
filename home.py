15b6211452000717734d69a0d33c204654097b90
jamiehax <jamieh028@gmail.com>
2024-10-14 13:52:58 -0400

    add PID checker page

--- home.py
+++ home.py
@@ -25,13 +25,17 @@ st.markdown("#### SDG Data Dashboard")
 st.markdown("Use the SDG Dashboard for exploratory data analysis of the United Nations Sustainable Development Goals DataBase. Explore the 17 sustainable development goals and their corresponding indicators, and select and download data by indicator, country, and time range. Create automatic interactive time series graphs on the selected data. Use the AI chatbot to understand the dataset with natural language questions. Use the Mitosheet Spreadsheet as an embedded spreadsheet to manipulate data and create visualizations. Use the PyGWalker Data Visualization tool to create visualizations from the data.")
 st.page_link("sdg_dashboard.py", label="SDG Dashboard", icon=":material/show_chart:", use_container_width=True)
 
+st.markdown("#### OSAA General Chatbot")
+st.markdown("The OSAA General Chatbot is similar to ChatGPT, except it has context specific to OSAA. Use it for questions specific to OSAA work.")
+st.page_link("chatbot.py", label="OSAA General Chatbot", icon=":material/chat:", use_container_width=True)
+
 st.markdown("#### Contradictory Analysis Tool")
 st.markdown("Use the Contradictory Analysis Tool to check if analysis contradicts any previous in OSAA's publications. This tool uses large language models with retrieval augmented generation and therefore may provide wrong answers, so alwayds double check.")
 st.page_link("check_analysis.py", label="Contradictory Analysis Tool", icon=":material/check:", use_container_width=True)
 
-st.markdown("#### OSAA General Chatbot")
-st.markdown("The OSAA General Chatbot is similar to ChatGPT, except it has context specific to OSAA. Use it for questions specific to OSAA work.")
-st.page_link("chatbot.py", label="OSAA General Chatbot", icon=":material/chat:", use_container_width=True)
+st.markdown("#### PID Checker")
+st.markdown("Use the PID Checker to upload a PID and see if it aligns with the PID criteria.")
+st.page_link("pid_checker.py", label="PID Checker", icon=":material/task_alt:", use_container_width=True)
 
 # st.markdown("<hr>", unsafe_allow_html=True)
 # st.write("")
